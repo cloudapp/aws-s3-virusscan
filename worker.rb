092963b50e1522330aab2e851fa5e6597da897f9
Al Pal <superrawr@gmail.com>
2016-08-03 18:54:43 -0700

    cleanup some code style in worker.rb
    - do not need to nest for body.key?(‘Record’); just `next`.
    - add some comments about the scanning.

--- worker.rb
+++ worker.rb
@@ -19,70 +19,72 @@ log.info "s3-virusscan started"
 
 poller.poll do |msg|
   body = JSON.parse(msg.body)
-  if body.key?('Records')
-    body['Records'].each do |record|
-      bucket = record['s3']['bucket']['name']
-      key = URI.decode(record['s3']['object']['key']).gsub('+', ' ')
-      log.debug "scanning s3://#{bucket}/#{key}..."
-      begin
-        s3.get_object(
-          response_target: '/tmp/target',
-          bucket: bucket,
-          key: key
-        )
-      rescue Aws::S3::Errors::NoSuchKey
-        log.debug "s3://#{bucket}/#{key} no longer exists. Skipping..."
-        next
-      end
+  next if !body.key?('Records')
 
-      system('clamscan --max-filesize=100M --max-scansize=500M /tmp/target')
-      result = $?.exitstatus
-      log.debug "clamscan exit code = #{result}"
+  # Scan each record available.
+  body['Records'].each do |record|
+    # Set bucket and key for getting a bucket item.
+    bucket = record['s3']['bucket']['name']
+    key = URI.decode(record['s3']['object']['key']).gsub('+', ' ')
 
-      if result == 0
-        log.debug "s3://#{bucket}/#{key} was scanned without findings"
-      elsif result == 2
-        log.debug "ClamAV had an issue and couldn't/didn't scan the file. Skipped #{key}"
-      elsif result == 1
-        if conf['delete']
-          log.error "s3://#{bucket}/#{key} is infected, deleting..."
-          sns.publish(
-            topic_arn: conf['topic'],
-            message: "s3://#{bucket}/#{key} is infected, deleting...",
-            subject: "s3-virusscan s3://#{bucket}",
-            message_attributes: {
-              "key" => {
-                data_type: "String",
-                string_value: "s3://#{bucket}/#{key}"
-              }
-            }
-          )
-          begin
-            s3.delete_object(
-              bucket: bucket,
-              key: key
-            )
-            log.error "s3://#{bucket}/#{key} was deleted"
-          rescue Exception => ex
-            log.error "Caught #{ex.class} error calling delete_object on #{key}. De-queueing anyway." 
-          end
-        else
-          log.error "s3://#{bucket}/#{key} is infected"
-          sns.publish(
-            topic_arn: conf['topic'],
-            message: "s3://#{bucket}/#{key} is infected",
-            subject: "s3-virusscan s3://#{bucket}",
-            message_attributes: {
-              "key" => {
-                data_type: "String",
-                string_value: "s3://#{bucket}/#{key}"
-              }
-            }
-          )
-        end
-      end
-      system('rm /tmp/target')
+    log.debug "scanning s3://#{bucket}/#{key}..."
+
+    # Ensure scannable object exists.
+    begin
+      s3.get_object(response_target: '/tmp/target', bucket: bucket, key: key)
+    rescue Aws::S3::Errors::NoSuchKey
+      log.debug "s3://#{bucket}/#{key} no longer exists. Skipping..."
+      next
     end
+
+    # Scan the target and check the result's exit status.
+    system('clamscan --max-filesize=100M --max-scansize=500M /tmp/target')
+    result = $?.exitstatus
+    log.debug "clamscan exit code = #{result}"
+
+    if result == 0
+      log.debug "s3://#{bucket}/#{key} was scanned without findings"
+    elsif result == 2
+      log.debug "ClamAV had an issue and couldn't/didn't scan the file. Skipped #{key}"
+    elsif result == 1
+      if conf['delete']
+        log.error "s3://#{bucket}/#{key} is infected, deleting..."
+        sns.publish(
+          topic_arn: conf['topic'],
+          message: "s3://#{bucket}/#{key} is infected, deleting...",
+          subject: "s3-virusscan s3://#{bucket}",
+          message_attributes: {
+            "key" => {
+              data_type: "String",
+              string_value: "s3://#{bucket}/#{key}"
+            }
+          }
+        )
+        begin
+          s3.delete_object(
+            bucket: bucket,
+            key: key
+          )
+          log.error "s3://#{bucket}/#{key} was deleted"
+        rescue Exception => ex
+          log.error "Caught #{ex.class} error calling delete_object on #{key}. De-queueing anyway." 
+        end
+      else
+        log.error "s3://#{bucket}/#{key} is infected"
+        sns.publish(
+          topic_arn: conf['topic'],
+          message: "s3://#{bucket}/#{key} is infected",
+          subject: "s3-virusscan s3://#{bucket}",
+          message_attributes: {
+            "key" => {
+              data_type: "String",
+              string_value: "s3://#{bucket}/#{key}"
+            }
+          }
+        )
+      end
+    end
+    system('rm /tmp/target')
   end
 end
 
